556f804250746fdb948bf3f2c491282fb655ee8b
Daniel Falbo <39460524+danielfalbo@users.noreply.github.com>
2023-05-01 19:57:28 +0200

    v0.0.5 Release

--- app/src/main/java/blockchainlab/constants/Constants.java
+++ app/src/main/java/blockchainlab/constants/Constants.java
@@ -1,7 +1,7 @@
 package blockchainlab.constants;
 
 public final class Constants {
-    public static final String VERSION = "0.0.5 dev";
+    public static final String VERSION = "0.0.5";
 
     public static final String BTC = "₿";
     public static final String BLOCKCHAIN_LAB_STRING = BTC + "lockchain Lab";
