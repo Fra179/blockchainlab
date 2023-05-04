32a95e095519f346f9177ec6f6c9c4285e76f86a
Daniel Falbo <39460524+danielfalbo@users.noreply.github.com>
2023-05-04 16:30:12 +0200

    Comment Miner.java

--- app/src/main/java/blockchainlab/blockchain/Miner.java
+++ app/src/main/java/blockchainlab/blockchain/Miner.java
@@ -4,7 +4,14 @@ public final class Miner {
     private Miner() {
     }
 
-    // TODO: javadoc
+    /**
+     * Mines a Block returning an HashedBlock
+     * whose hash starts with as many zeros as the specified difficulty
+     * 
+     * @param block      block to mine
+     * @param difficulty number of zeros the hash of the result should start with
+     * @return hashed block with hash starting with `difficulty` zeros
+     */
     public static final HashedBlock mine(Block block, int difficulty) {
         int nonce = 0;
         HashableBlock hashableBlock = new HashableBlock(block, nonce++);
