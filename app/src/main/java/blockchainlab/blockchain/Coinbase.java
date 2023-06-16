33ba327880873b2b3fac800e3fb3dadeadd4df4b
Daniel Falbo <39460524+danielfalbo@users.noreply.github.com>
2023-06-16 16:20:39 +0200

    Add Coinbase.java

---
+++ app/src/main/java/blockchainlab/blockchain/Coinbase.java
@@ -0,0 +1,16 @@
+package blockchainlab.blockchain;
+
+public class Coinbase {
+    public final double amount;
+    public final Wallet wallet;
+
+    public Coinbase(Double amount, Wallet wallet) {
+        this.amount = amount;
+        this.wallet = wallet;
+    }
+
+    public Coinbase(Double amount, String address) {
+        this.amount = amount;
+        this.wallet = new Wallet(address);
+    }
+}
