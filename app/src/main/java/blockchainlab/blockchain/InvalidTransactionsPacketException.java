eceae48b68d5625b8a0851e12178d785b14c9282
Daniel Falbo <39460524+danielfalbo@users.noreply.github.com>
2023-06-16 17:08:02 +0200

    Add InvalidTransactionsPacketException

---
+++ app/src/main/java/blockchainlab/blockchain/InvalidTransactionsPacketException.java
@@ -0,0 +1,12 @@
+package blockchainlab.blockchain;
+
+public class InvalidTransactionsPacketException extends Exception {
+    public InvalidTransactionsPacketException(String message) {
+        super(message);
+    }
+
+    public static final InvalidTransactionsPacketException PACKET_SIZE_DOESNT_MATCH_STANDARD_SIZE_EXCEPTION = new InvalidTransactionsPacketException(
+            "Packet size doesn't match standard size.");
+    public static final InvalidTransactionsPacketException PACKET_CONTAINS_NULL_TRANSACTION_EXCEPTION = new InvalidTransactionsPacketException(
+            "Packet contains null transaction");
+}
