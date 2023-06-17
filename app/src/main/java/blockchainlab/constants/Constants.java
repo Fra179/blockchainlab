b8609bc96ffc30eae2efaefa5c8f29c82ef761f3
Daniel Falbo <39460524+danielfalbo@users.noreply.github.com>
2023-06-17 11:09:02 +0200

    [release gh action] auto bump brew formula

--- app/src/main/java/blockchainlab/constants/Constants.java
+++ app/src/main/java/blockchainlab/constants/Constants.java
@@ -1,7 +1,7 @@
 package blockchainlab.constants;
 
 public final class Constants {
-    public static final String VERSION = "0.0.6 dev";
+    public static final String VERSION = "0.0.7 dev";
 
     public static final String BTC = "₿";
     public static final String BLOCKCHAIN_LAB_STRING = BTC + "lockchain Lab";
